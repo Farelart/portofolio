545438dcc4673a4992daa395b6d53a882b745015
Farelart <ffjdganlaky@gmail.com>
2024-12-17 06:29:14 +0100

    some interesting changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,45 +1,9 @@
-import Link from "next/link";
-import { getBlogPosts } from "@/data/blog";
-import { BLUR_FADE_DELAY } from "@/data/constants";
-import BlurFade from "@/components/magicui/blur-fade";
+import React from 'react'
 
-export const metadata = {
-  title: "Blog - @yyassif",
-  description: "My thoughts on software development, life, and so many more.",
-};
-
-export default async function BlogPage() {
-  const posts = await getBlogPosts();
-
+export default function page() {
   return (
-    <section>
-      <BlurFade delay={BLUR_FADE_DELAY}>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter"><Link href="/blog">@yyassif</Link> Blog</h1>
-      </BlurFade>
-      {posts
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1;
-          }
-          return 1;
-        })
-        .map((post, id) => (
-          <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
-            <Link
-              className="flex flex-col space-y-1 mb-4"
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col">
-                <p className="tracking-tight">{post.metadata.title}</p>
-                <p className="h-6 text-xs text-muted-foreground">
-                  {post.metadata.publishedAt}
-                </p>
-              </div>
-            </Link>
-          </BlurFade>
-        ))}
-    </section>
-  );
+    <div>
+      
+    </div>
+  )
 }
