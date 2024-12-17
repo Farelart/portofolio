bcc4ebe02900732736d81dfba638fee16033b4d1
Farelart <ffjdganlaky@gmail.com>
2024-12-17 14:41:02 +0100

    some fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -345,32 +345,13 @@ export default function Page() {
                   </h2>
                   <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                     During my time in university, I attended{" "}
-                    {DATA.hackathons.length}+ hackathons both as participant &/
-                    mentor. People from around the country would come together
-                    and build incredible things in 2-3 days.
+                    several hackathons both as participant &/
+                    mentor and present one research project at an international symposium.
                   </p>
                 </div>
               </div>
             </BlurFade>
-            <BlurFade delay={BLUR_FADE_DELAY * 14}>
-              <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-                {DATA.hackathons.map((project, id) => (
-                  <BlurFade
-                    key={project.title + project.dates}
-                    delay={BLUR_FADE_DELAY * 15 + id * 0.05}
-                  >
-                    <HackathonCard
-                      title={project.title}
-                      description={project.description}
-                      location={project.location}
-                      dates={project.dates}
-                      image={project.image}
-                      links={project.links}
-                    />
-                  </BlurFade>
-                ))}
-              </ul>
-            </BlurFade>
+           
           </div>
         </section>
         <section id="contact">
